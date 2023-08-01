9d3c7a7a8adb7549a903eaee5437c1cf7a1c5e9b
Pavel Kovalew <deadfred14@mail.ru>
2023-08-01 20:24:50 +0300

    Решение первого задания

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        sumOfPrimeNumbers(x);
+    }
+
+    public static void sumOfPrimeNumbers(int x) {
+        int sum = 1;
+        if (x > 0) {
+            for (int i = 1; i <= x; i++) {
+                int y = 0;
+                for (int a = 1; a <= i; a++) {
+                    if (i % a == 0) {
+                        y++;
+                    }
+                }
+                if (y == 2) {
+                    sum += i;
+                }
+            }
+            System.out.println(sum);
+        } else {
+            System.out.println("Х должен быть больше нуля");
+        }
+    }
+}
